8989f6f978b13ea100155fc62171c78d12cf241b
将来过往 <zhangjiZHJ@outlook.com>
2018-08-10 14:44:46 +0800

    笔试基础模板

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <chrono>
+#include <iterator>
+
+using namespace std;
+
+map<string, string> maps;
+vector<string> tobefind;
+
+int fun(bool )
+{
+
+    return true;
+}
+int main(int argc, char const *argv[])
+{
+    int T, n;
+    cin >> T;
+    while (T--)
+    {
+        bool flag = true;
+        if (T == 0)
+        {
+            flag = false;
+        }
+        fun(flag);
+    }
+
+    // while (n--)
+    // {
+    //     string T;
+    //     cin >> T;
+    //     if (fun(T))
+    //     {
+    //         cout << "yes";
+    //     }
+    //     else
+    //     {
+    //         cout << "no";
+    //     }
+    //     if (n != 0)
+    //     {
+    //         cout << endl;
+    //     }
+    // }
+    return 0;
+}
